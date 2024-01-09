764393991a78606f9eb6d491dbd25d3cd896ca53
chistym17 <mchisty31@gmail.com>
2024-01-09 16:19:03 +0600

    search functionality  added

--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -1,13 +1,11 @@
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
-import Feed from '../components/Feed';
 
 const Main = () => {
   return (
     <div>
       <Navbar />
       <Outlet /> 
-      <Feed />
     </div>
   );
 };
